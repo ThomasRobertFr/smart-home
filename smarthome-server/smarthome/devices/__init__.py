1b443bacdac26d687316d0415b894b3d6c07865a
Thomas Robert <thomas.robert@heuritech.com>
2021-05-14 14:37:38 +0200

    Fix isort of devices __init__ file

--- smarthome-server/smarthome/devices/__init__.py
+++ smarthome-server/smarthome/devices/__init__.py
@@ -1,12 +1,12 @@
+# isort:skip_file
+
+from ._Generic import Device, Devices, Dimmer, PushButton, Switch
 from ._ESPCustomDimmer import ESPCustomDimmer
 from ._ESPEasyLights import ESPEasyDimmer
 from ._EverydayCalendar import EverydayCalendar
-from ._Generic import Device, Devices, Dimmer, PushButton, Switch
 from ._HueLamp import HueLamp
 from ._IRRemote import IRRemote
 from ._NAS import NAS
 from ._RFRemote import RFRemote
-
-# from ._Watering import WateringSensor, WateringSensors
 
 # from ._Crespin import Crespin
