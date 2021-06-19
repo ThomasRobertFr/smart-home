72d2770cbb59fec1f85e040e353d97af23b970b0
Thomas Robert <thomas.robert@heuritech.com>
2021-06-19 22:34:49 +0200

    Add 4th watering device config

---
+++ esp8266-watering/include/device4.h
@@ -0,0 +1,7 @@
+#include <Arduino.h>
+
+int pinCalibrationLED = LED_BUILTIN;
+int nbSensors = 2;
+String sensorIds[2] = {"jardiniere", "ciboulette"};
+int pinSensors[2] = {D1, D2}; // {D1, D2, D3};
+int pinMotors[2] = {LED_BUILTIN, LED_BUILTIN}; // {D5, D6, D7};
