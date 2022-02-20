b7a59babe01bdd249770132c5054f4f44476b453
Thomas Robert <thomas.robert@heuritech.com>
2022-02-20 22:04:47 +0100

    Small changes in smart home server

--- smarthome-server/smarthome/webui/js/generic.js
+++ smarthome-server/smarthome/webui/js/generic.js
@@ -4,9 +4,9 @@ const dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Frida
 const dayNamesShort = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 const monthNamesShort = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-const apiURL = "http://192.168.1.10/api/"
-const calendarURL = "http://192.168.1.10/calendar/"
-const domoticzAPI = "http://192.168.1.10:8080/json.htm?"
+const apiURL = "/api/"
+const calendarURL = "/calendar/"
+const domoticzAPI = "/domoticz/json.htm?"
 
 // updates to perform [fct, interval (s), lastUpdateField]
 var updates = [];
